5deac26229068ff4ac96ada0fb5b33a66ee963e6
Alex Kiura <kiuraalex@gmail.com>
2023-11-21 04:39:43 +0300

    Construct sql table creation

--- orm.py
+++ orm.py
@@ -1,4 +1,8 @@
+import inspect
 import sqlite3
+
+
+# TODO: Use jinja2 templates for constructing SQL
 
 
 class Database:
@@ -9,9 +13,27 @@ class Database:
     def tables(self):
         return []
 
+    def create(self, table):
+        self.connection.execute(table._get_create_sql())
+
 
 class Table:
-    ...
+    @classmethod
+    def _get_create_sql(cls):
+        CREATE_TABLE_SQL = "CREATE TABLE IF NOT EXISTS {name} ({fields});"
+        fields = [
+            "id INTEGER PRIMARY KEY AUTOINCREMENT",
+        ]
+
+        for column_name, field in inspect.getmembers(cls):
+            if isinstance(field, Column):
+                fields.append(f"{column_name} {field.sql_type}")
+            elif isinstance(field, ForeignKey):
+                fields.append(f"{column_name}_id INTEGER")
+
+        fields = ", ".join(fields)
+        table_name = cls.__name__.lower()
+        return CREATE_TABLE_SQL.format(name=table_name, fields=fields)
 
 
 class Column:
