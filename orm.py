5b655778dbe794d5b7a114853328e8292d12b55b
Alex Kiura <kiuraalex@gmail.com>
2023-11-23 14:28:58 +0300

    Query all rows in a table

--- orm.py
+++ orm.py
@@ -1,36 +1,46 @@
 import inspect
 import sqlite3
-from typing import Tuple, List
+from typing import Tuple, List, Union
 
 
 # TODO: Use jinja2 templates for constructing SQL queries
+
+Value = Union[str, int, None, float, bytes, bool]
 
 
 class Database:
     def __init__(self, path: str):
         self.connection = sqlite3.Connection(path)
 
-    def create(self, table):
+    def create(self, table: type["Table"]):
         self.connection.execute(table._get_create_sql())
 
     def save(self, instance: "Table"):
         sql, values = instance._get_insert_sql()
-        cursor = self.connection.execute(sql, values)
+        cursor: sqlite3.Cursor = self.connection.execute(sql, values)
         instance._data["id"] = cursor.lastrowid
         self.connection.commit()
 
-    def all(self, table: "Table"):
-        pass
+    def all(self, table: type["Table"]) -> List["Table"]:
+        sql, fields = table._get_select_all_sql()
+
+        results = []
+        for row in self.connection.execute(sql).fetchall():
+            instance = table()
+            for field, value in zip(fields, row):
+                setattr(instance, field, value)
+            results.append(instance)
+        return results
 
     @property
-    def tables(self):
+    def tables(self) -> List[str]:
         SELECT_TABLES_SQL = "SELECT name FROM sqlite_master WHERE type = 'table';"
-        return {x[0] for x in self.connection.execute(SELECT_TABLES_SQL).fetchall()}
+        return [x[0] for x in self.connection.execute(SELECT_TABLES_SQL).fetchall()]
 
 
 class Table:
     def __init__(self, **kwargs) -> None:
-        self._data = {
+        self._data: dict[str, Value] = {
             "id": None
         }
         for key, value in kwargs.items():
@@ -43,7 +53,7 @@ class Table:
         return super().__getattribute__(key)
 
     @classmethod
-    def _get_create_sql(cls):
+    def _get_create_sql(cls) -> str:
         CREATE_TABLE_SQL = "CREATE TABLE IF NOT EXISTS {name} ({fields});"
         fields = [
             "id INTEGER PRIMARY KEY AUTOINCREMENT",
@@ -59,7 +69,7 @@ class Table:
         table_name = cls.__name__.lower()
         return CREATE_TABLE_SQL.format(name=table_name, fields=fields)
 
-    def _get_insert_sql(self) -> Tuple[str, List]:
+    def _get_insert_sql(self) -> Tuple[str, List[Value]]:
         INSERT_SQL = "INSERT INTO {name} ({fields}) VALUES ({placeholders});"
         cls = self.__class__
         fields, placeholders, values = [], [], []
@@ -84,15 +94,33 @@ class Table:
         )
         return sql, values
 
+    @classmethod
+    def _get_select_all_sql(cls) -> Tuple[str, List[str]]:
+        SELECT_ALL_SQL = "SELECT {fields} FROM {name};"
+
+        fields = ["id"]
+        for name, field in inspect.getmembers(cls):
+            if isinstance(field, Column):
+                fields.append(name)
+            if isinstance(field, ForeignKey):
+                fields.append(name + "_id")
+
+        sql = SELECT_ALL_SQL.format(
+            name=cls.__name__.lower(),
+            fields=", ".join(fields)
+        )
+
+        return sql, fields
+
 
 class Column:
 
-    def __init__(self, column_type) -> None:
+    def __init__(self, column_type: type) -> None:
         self._type = column_type
 
     @property
-    def sql_type(self):
-        TYPE_MAP = {
+    def sql_type(self) -> str:
+        TYPE_MAP: dict[type, str] = {
             int: "INTEGER",
             float: "REAL",
             str: "TEXT",
