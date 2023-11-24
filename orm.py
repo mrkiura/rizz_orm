8d2c0f8ccc21b829a41dd71cc3e646f81eb4e404
Alex Kiura <kiuraalex@gmail.com>
2023-11-24 20:44:05 +0300

    Implement and test update

--- orm.py
+++ orm.py
@@ -35,6 +35,11 @@ class Database:
         instance._data["id"] = cursor.lastrowid
         self.connection.commit()
 
+    def update(self, instance):
+        sql, values = instance._get_update_sql()
+        self.connection.execute(sql, values)
+        self.connection.commit()
+
     def all(self, table: type["Table"]) -> List["Table"]:
         sql, fields = table._get_select_all_sql()
 
@@ -148,6 +153,27 @@ class Table:
         sql = SELECT_WHERE_SQL.format(name=cls.__name__.lower(), fields=", ".join(fields))
         params = [id]
         return sql, fields, params
+
+    def _get_update_sql(self):
+        UPDATE_SQL = "UPDATE {name} SET {fields} WHERE id = ?"
+        cls = self.__class__
+        fields, values = [], []
+
+        for name, field in inspect.getmembers(cls):
+            if isinstance(field, Column):
+                fields.append(name)
+                values.append(getattr(self, name))
+            elif isinstance(field, ForeignKey):
+                fields.append(name + "_id")
+                values.append(getattr(self, name).id)
+        values.append(getattr(self, "id"))
+
+        sql = UPDATE_SQL.format(
+            name=cls.__name__.lower(),
+            fields=", ".join([f"{field} = ?" for field in fields])
+        )
+
+        return sql, values
 
 
 class Column:
